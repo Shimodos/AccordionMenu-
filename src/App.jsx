ce52fc0b0bd583f44a5ab4225196b29e282e1795
Alekssey Demchenko <ad@pesochin.net>
2024-04-21 01:23:40 +0300

    Build an Accordion Menu

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import FAQ from './components/FAQ';
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-gradient-to-r from-indigo-500 to-blue-500">
+      <div className="p-4">
+        <FAQ />
+        <h1>Accordion Menu</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
